9233823cd2597705565b7d1f7ec8c776b279d288
Nguyễn Hữu Duy Kha <94631848+dkhak3@users.noreply.github.com>
2024-01-14 17:05:07 +0700

    ok

--- App.js
+++ App.js
@@ -4,19 +4,22 @@ import { StyleSheet, Text, View, TextInput } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.containerTop}>
-        <View style={styles.top}>
-          <Text style={styles.topText}>Top Left</Text>
-        </View>
+      <View style={[styles.center, styles.top]}>
+        <Text style={[styles.text, styles.topText]}>Top Left</Text>
       </View>
-      <View style={styles.containerCenter}>
-        <View style={styles.center}>
-          <Text style={styles.centerText}>Center</Text>
+      <View style={styles.bottom}>
+        <View style={[styles.center, styles.bottomLeft]}>
+          <Text style={[styles.text, styles.bottomText]}>Bottom Left</Text>
         </View>
-      </View>
-      <View style={styles.containerBottom}>
-        <View style={styles.bottom}>
-          <Text style={styles.bottomText}>Bottom Right</Text>
+        <View style={styles.bottomRight}>
+          <View style={[styles.center, styles.bottomRightTop]}>
+            <Text style={[styles.text, styles.bottomTextTop]}>Right Top</Text>
+          </View>
+          <View style={[styles.center, styles.bottomRightBottom]}>
+            <Text style={[styles.text, styles.bottomTextBottom]}>
+              Right Bottom
+            </Text>
+          </View>
         </View>
       </View>
     </View>
@@ -25,61 +28,49 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#E7FEFE",
     flex: 1,
   },
-  containerTop: {
-    flex: 1,
-  },
-  containerCenter: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  containerBottom: {
-    flex: 1,
-  },
-  bottom: {
-    marginTop: 80,
-    marginLeft: 40,
-    marginRight: 40,
-    backgroundColor: "#67BCD0",
-    padding: 15,
-    borderRadius: 20,
-  },
-  bottomText: {
-    color: "#fff",
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "right",
-  },
-  top: {
-    marginTop: 80,
-    marginLeft: 40,
-    marginRight: 40,
-    backgroundColor: "#fff",
-    padding: 15,
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "red",
-    borderRadius: 5,
-  },
-  topText: {
-    color: "#63AEC6",
+  text: {
     fontSize: 20,
     fontWeight: "bold",
   },
   center: {
-    width: 150,
-    height: 150,
-    backgroundColor: "#32FF61",
-    borderRadius: 100,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
   },
-  centerText: {
+  top: {
+    flex: 1,
+    backgroundColor: "#F4FE87",
+  },
+  topText: {
     color: "#63AEC6",
-    fontSize: 20,
-    fontWeight: "bold",
+  },
+  bottom: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  bottomLeft: {
+    backgroundColor: "#3DEE84",
+    flex: 1,
+  },
+  bottomText: {
+    color: "#63AEC6",
+  },
+  bottomRight: {
+    flex: 1,
+  },
+  bottomRightTop: {
+    backgroundColor: "#EF3DE7",
+    flex: 2,
+  },
+  bottomTextTop: {
+    color: "#fff",
+  },
+  bottomRightBottom: {
+    backgroundColor: "#2C83B2",
+    flex: 1,
+  },
+  bottomTextBottom: {
+    color: "#fff",
   },
 });
